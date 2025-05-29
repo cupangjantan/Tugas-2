75b409af5bc6e72bf8029487b8108f2f1003508a
cupangjantan <evaahard@gmail.com>
2025-05-29 13:25:32 +0700

    Update kursus.sql
    perbaiki sedikit

--- kursus.sql
+++ kursus.sql
@@ -2,14 +2,14 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
+-- Host: localhot
 -- Waktu pembuatan: 29 Bulan Mei 2025 pada 03.37
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+00:00";
+SET time_zone = "0";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
